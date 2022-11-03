6d72b61a379254fc0421a8725d3adba5e5d487e2
Pierre Burghardt <pierre.burghardt@student.hpi.uni-potsdam.de>
2022-11-03 22:33:48 +0100

    Issue 2, new author form; implemented test and layout of new.html.erb

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,9 @@ describe "New author page", type: :feature do
     # https://guides.rubyonrails.org/routing.html#pbundlath-and-url-helpers
     visit new_author_path
   end
+  it "should exist a text input field for an authors first- and last name and his homepage" do
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
+  end
 end
