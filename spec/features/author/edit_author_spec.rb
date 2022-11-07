939988acd17f3a990f63250eab4a38e5c1f98b71
Pierre Burghardt <pierre.burghardt@student.hpi.uni-potsdam.de>
2022-11-07 14:03:31 +0100

    Issue 6, fix test: now using factoryBot and visiting real edit page

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe "Edit author page", type: :feature do
   it "should exist at 'edit_author_path' and render withour error" do
-    visit edit_author_path(Author.first)
+    @alan = FactoryBot.create :author
+
+    visit edit_author_path(@alan)
   end
 
 end
