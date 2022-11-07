ec13429ed129b71fa656f1276e4509c01cf130af
Pierre Burghardt <pierre.burghardt@student.hpi.uni-potsdam.de>
2022-11-07 12:53:41 +0100

    Issue 6, add error message to newHtmlPage, test failing

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,12 +5,14 @@ describe "New author page", type: :feature do
     # https://guides.rubyonrails.org/routing.html#pbundlath-and-url-helpers
     visit new_author_path
   end
+
   it "should have a text input field for an authors first- and last name and his homepage" do
     visit new_author_path
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
   it "should show an error message if the form is submitted without a last name" do
     visit new_author_path
     fill_in 'author[first_name]', with: 'Alan'
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,6 +4,7 @@ describe "Author Model", :type => :model do
     author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
     expect(author.first_name).to eq("Alan")
   end
+
   it "should not pass validation with empty last name" do
     author = Author.new(first_name: "Alan", last_name: "", homepage: "http://wikipedia.org/Alan_Turing")
     expect(author).to_not be_valid
