b4c8ab465afe3fd5ccadc884210babe1c49f1a57
Pierre Burghardt <pierre.burghardt@student.hpi.uni-potsdam.de>
2022-11-07 14:36:30 +0100

    Issue 9, add IndexPage test for edit link, add Edit Link at IndexHtml and ShowHtml

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe "Index Author Page", type: :feature do
+
   it "should show all saved Authors" do
     visit authors_path
 
@@ -8,4 +9,14 @@ describe "Index Author Page", type: :feature do
     expect(page).to have_text("Homepage")
     expect(page).to have_link 'New', href: new_author_path
   end
+
+  it "should have a link to the edit page" do
+    @alan = FactoryBot.create :author
+    visit authors_path
+
+    expect(page).to have_text("Alan Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+    expect(page).to have_link 'Edit', href: edit_author_path(@alan)
+  end
+
 end
