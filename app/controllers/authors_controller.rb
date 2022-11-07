e1e45d04737efd2ab7cef1d2341721b93701d2d3
Pierre Burghardt <pierre.burghardt@student.hpi.uni-potsdam.de>
2022-11-07 14:24:52 +0100

    Issue 8, fix editHtml, test running (existence)

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -20,7 +20,16 @@ class AuthorsController < ApplicationController
     else 
       render 'new'
     end
-    
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render 'edit'
+    end
   end
 
   private
