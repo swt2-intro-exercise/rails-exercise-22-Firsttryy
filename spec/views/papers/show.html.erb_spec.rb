2cf5ea4da8211004f86d908704d3f6fc0cf140b4
Pierre Burghardt <pierre.burghardt@student.hpi.uni-potsdam.de>
2022-11-08 22:25:16 +0100

    Issue 15: add test -paperShow should show author name-

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -16,3 +16,17 @@ RSpec.describe "papers/show", type: :view do
     expect(rendered).to match(/2/)
   end
 end
+
+describe "My Paper Show Page", type: :feature do
+  it "should render without error" do
+    paper = FactoryBot.create :paper
+    visit paper_path(paper)
+  end
+
+  it "should show the authors full name given an author" do
+    alan = FactoryBot.create :author
+    paper = Paper.create(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950, authors: [alan])
+    visit paper_path(paper)
+    expect(page).to have_text(alan.name)
+  end
+end
