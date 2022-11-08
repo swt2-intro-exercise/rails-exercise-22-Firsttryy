8ee8a8571403658ef8e529f3844ed2ba7b9f6df0
Pierre Burghardt <pierre.burghardt@student.hpi.uni-potsdam.de>
2022-11-08 18:16:42 +0100

    add Delete test for papersIndexPage

--- spec/views/papers/index_paper_spec.rb
+++ spec/views/papers/index_paper_spec.rb
@@ -4,14 +4,14 @@ describe "papers/index", type: :view do
   before(:each) do
     assign(:papers, [
       Paper.create!(
-        title: "Title",
-        venue: "Venue",
-        year: 2
+        title: "COMPUTING MACHINERY AND INTELLIGENCE",
+        venue: "Mind 49: 433-460",
+        year: 1950
       ),
       Paper.create!(
-        title: "Title",
-        venue: "Venue",
-        year: 2
+        title: "COMPUTING MACHINERY AND INTELLIGENCE",
+        venue: "Mind 49: 433-460",
+        year: 1950
       )
     ])
   end
@@ -19,8 +19,17 @@ describe "papers/index", type: :view do
   it "renders a list of papers" do
     render
     cell_selector = 'tr>td'
-    assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Venue".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("COMPUTING MACHINERY AND INTELLIGENCE".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Mind 49: 433-460".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(1950.to_s), count: 2
+  end
+
+  it "should have a link to delete a paper" do
+    render
+
+    expect(page).to have_text("COMPUTING MACHINERY AND INTELLIGENCE")
+    expect(page).to have_text("Mind 49: 433-460")
+    expect(page).to have_text("1950")
+    expect(page).to have_link 'Delete', href: paper_path(@itBook)
   end
 end
